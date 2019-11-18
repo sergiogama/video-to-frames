07380535629073e1e6ddde0fd70ac0cf83432e76
danitrod <danitrod@ibm.com>
2019-11-18 16:39:48 -0300

    Update frames/second

--- v2f.py
+++ v2f.py
@@ -5,6 +5,6 @@ count = 0
 success = True
 while success:
     success, image = vidcap.read()
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*10000)) # Um frame a cada 1000 ms (1f/segundo)
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000)) # Um frame a cada 1000 ms (1f/segundo)
     cv2.imwrite("frames/frame%d.jpg" % count, image) # save frame as JPEG file
     count += 1
